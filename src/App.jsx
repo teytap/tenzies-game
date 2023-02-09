89a95e76fe82f62af122f06522b83094c651231c
Mehtap Tataroğlu <teytap@gmail.com>
2023-02-10 01:27:54 +0300

    added comments and changed style

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,23 @@ export default function App() {
   const [tenzies, setTenzies] = React.useState(false);
   const [count, setCount] = React.useState(1);
 
+  const [time, setTime] = React.useState(0);
+  const [start, setStart] = React.useState(Date.now());
+  let passedTime = Math.round((time - start) / 1000);
+
   React.useEffect(() => {
     // if every die isHeld and all the values are same game is won
     const allHeld = dice.every((die) => die.isHeld);
+    //every checks each
     const firstValue = dice[0].value;
     const allSameValue = dice.every((die) => die.value === firstValue);
+
     if (allHeld && allSameValue) {
       setTenzies(true);
+      setTime(Date.now());
+      localStorage.setItem("bestTime", passedTime);
     }
   }, [dice]);
-
   function newDice() {
     //generates dice array of 6 objects with die properties
     let diceArray = [];
@@ -36,7 +43,7 @@ export default function App() {
   function rollDice() {
     //to count how many rolls at win!
     setCount(count + 1);
-    console.log(count);
+
     //when game finished if condition helps to start new game
     if (!tenzies) {
       setDice((prevDice) =>
@@ -54,21 +61,24 @@ export default function App() {
     } else {
       setTenzies(false);
       setDice(newDice());
+      setCount(1);
+      setStart(Date.now());
     }
   }
 
+  //when die values are clicked function helps to hold/unhold and save
   function holdDice(id) {
-    //when die values are clicked function helps to hold/unhold and save
     setDice((prevDice) =>
       prevDice.map((die) => {
         return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
       })
     );
   }
+
   //this map takes dice elements and maps as die component with its own props
   const diceElements = dice.map((die) => (
     <Die
-      key={die.key}
+      key={die.id}
       value={die.value}
       isHeld={die.isHeld}
       holdDice={() => holdDice(die.id)}
@@ -80,12 +90,17 @@ export default function App() {
       {tenzies === true && <Confetti />}
       <div className="explanation">
         <h1>Tenzies</h1>
-        <p>
-          {/* changes instruction to you won! when game is won */}
-          {tenzies === true
-            ? `You won in ${count} rolls!`
-            : `Roll untill all dice are the same. Click each die to freeze it at its current value betwen rolls.`}
-        </p>
+
+        {/* changes instruction to you won! when game is won */}
+        {tenzies === true ? (
+          <div>
+            You won in <span className="score">{count}</span> rolls and{" "}
+            <span className="score">{passedTime}</span> seconds!
+          </div>
+        ) : (
+          `Roll until all dice are the same. Click each die to freeze it at
+              its current value between rolls.`
+        )}
       </div>
       <div className="dice">{diceElements}</div>
       {
