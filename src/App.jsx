bb7881f548bd6930e8cbbdad7459934864b63b8a
Mehtap Tataroğlu <teytap@gmail.com>
2023-06-25 15:39:12 +0300

    added best time

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,44 @@ export default function App() {
   const [bestCount, setBestCount] = React.useState(
     JSON.parse(localStorage.getItem("bestcount")) || 0
   );
-
+  //---time and best time definitions---
   const [time, setTime] = React.useState(Date.now());
   const [start, setStart] = React.useState(Date.now());
+  const [bestTime, setBestTime] = React.useState(
+    JSON.parse(localStorage.getItem("besttime")) || Infinity
+  );
 
-  const passedTime = Math.round((time - start) / 1000);
+  let passedTime = Infinity;
+  if (start < time) {
+    passedTime = Math.round((time - start) / 1000);
+  }
 
   React.useEffect(() => {
-    // if every die isHeld and all the values are same game is won
+    // ---if every die isHeld and all the values are same game is won---
     const allHeld = dice.every((die) => die.isHeld);
-    //every checks each
+    //---every checks each---
     const firstValue = dice[0].value;
     const allSameValue = dice.every((die) => die.value === firstValue);
 
     if (allHeld && allSameValue) {
       setTenzies(true);
       setTime(Date.now());
-      // stores the best score of rolls
-      localStorage.setItem(
-        "bestcount",
-        localStorage.getItem("bestcount") < count
-          ? localStorage.getItem("bestcount")
-          : count
-      );
+
+      //--- best count---
       if (count < bestCount || bestCount === 0) {
         localStorage.setItem("bestcount", count);
         setBestCount(count);
       }
+
+      if (passedTime < bestTime || bestTime === 0) {
+        setBestTime(passedTime);
+        localStorage.setItem("besttime", passedTime.toString());
+      }
     }
-  }, [dice]);
+  }, [dice, passedTime, bestTime]);
 
   function newDice() {
-    //generates dice array of 6 objects with die properties
+    //---generates dice array of 6 objects with die properties---
     let diceArray = [];
     for (let i = 0; i < 10; i++) {
       diceArray.push({
@@ -56,10 +62,10 @@ export default function App() {
   }
 
   function rollDice() {
-    //to count how many rolls at win!
+    //---to count how many rolls at win!---
     setCount(count + 1);
 
-    //when game finished if condition helps to start new game
+    //---when game finished if condition helps to start new game---
     if (!tenzies) {
       setDice((prevDice) =>
         prevDice.map((die) => {
@@ -68,7 +74,7 @@ export default function App() {
             : {
                 value: Math.floor(Math.random() * 6),
                 isHeld: false,
-                //nanoid component generates id for key
+                //---nanoid component generates id for key---
                 id: nanoid(),
               };
         })
@@ -81,7 +87,7 @@ export default function App() {
     }
   }
 
-  //when die values are clicked function helps to hold/unhold and save
+  //---when die values are clicked function helps to hold/unhold and save---
   function holdDice(id) {
     setDice((prevDice) =>
       prevDice.map((die) => {
@@ -90,7 +96,7 @@ export default function App() {
     );
   }
 
-  //this map takes dice elements and maps as die component with its own props
+  //---this map takes dice elements and maps as die component with its own props---
   const diceElements = dice.map((die) => (
     <Die
       key={die.id}
@@ -102,22 +108,23 @@ export default function App() {
 
   return (
     <main>
-      {/* confetti starts when game is won */}
+      {/* ---confetti starts when game is won--- */}
       {tenzies === true && <Confetti />}
       <div className="explanation">
         <h1>Tenzies</h1>
 
-        {/* changes instruction to you won! when game is won */}
+        {/* ---changes instruction to you won! when game is won--- */}
         {tenzies === true ? (
           <div>
             ⏱ You won in <span className="score">{count}</span> rolls and in{" "}
             <span className="score">{passedTime}</span> seconds!{" "}
-            {/* conditional rendering:if bestCount exists show the sentence */}
+            {/* ---conditional rendering:if bestCount exists show the sentence--- */}
             {bestCount !== 0 && (
-              <span>
+              <div>
                 🏆 Best score is in{" "}
-                <span className="best--score">{bestCount}</span> rolls.
-              </span>
+                <span className="best--score">{bestCount}</span> rolls, in
+                <span className="best--score"> {bestTime}</span> seconds!
+              </div>
             )}
           </div>
         ) : (
